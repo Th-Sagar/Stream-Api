23437398f0d3bcffab54fb874ba086f6fcac7e1e
Th-Sagar <sagarthapa98122@gmail.com>
2025-02-06 15:39:31 +0545

    feat: stream creation

--- src/main/java/StreamObject.java
+++ src/main/java/StreamObject.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamObject {
@@ -12,16 +14,39 @@ public class StreamObject {
         Stream<Object> emptyStream = Stream.empty();
 //        emptyStream.forEach(e-> System.out.println(e));
 
+        //2 array, object, collection
         String names[] ={
                 "Durgesh",
                 "Uttam",
                 "Ankit",
                 "Divya"
         };
+
         Stream<String> names1 = Stream.of(names);
         names1.forEach(x-> System.out.println(x));
 
         List<Integer> list = Arrays.asList(1, 2, 3, 4);
+
+        //3
+        Stream<Object> build = Stream.builder().build();
+
+
+        //4
+        IntStream stream = Arrays.stream(new int[]{2, 4, 65, 3, 564});
+
+        stream.forEach(x->
+                System.out.println(x));
+
+
+        //5. List, set
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(12);
+        list2.add(34);
+        list2.add(23);
+        list2.add(78);
+
+        Stream<Integer> stream1 = list2.stream();
+        stream1.forEach(x-> System.out.println(x));
 
 
 
