0b8e4518a8a2804deb5a483c4921ed2b63098326
Th-Sagar <sagarthapa98122@gmail.com>
2025-02-06 21:53:33 +0545

    feat: optional

--- src/main/java/cons/OptionalExample.java
+++ src/main/java/cons/OptionalExample.java
@@ -1,7 +1,35 @@
 package cons;
 
+import java.util.Optional;
+
 public class OptionalExample {
+
+//    String name;
+
+    public static Optional<String> getName(){
+        String name = "Durgesh";
+        return Optional.ofNullable(name);
+    }
+
+
     public static void main(String[] args) {
-        
+
+        String str = "Not null";
+//        if(str==null){
+//            System.out.println("This is null");
+//        }else {
+//            System.out.println(str.length());
+//        }
+
+
+        Optional<String> str1 = Optional.ofNullable(str); // create optional
+        System.out.println(str1.isPresent());
+//        System.out.println(str1.get());
+        System.out.println(str1.orElse("Null"));
+
+
+        Optional<String> name = getName();
+        System.out.println(name.orElse("Function return null"));
+
     }
 }
